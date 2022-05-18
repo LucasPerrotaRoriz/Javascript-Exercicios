528e3c12baa574c0863007cdac146ee6e268120b
LucasPerrotaRoriz <LucasPR1998@protonmail.com>
2022-05-18 13:51:09 -0300

    Javascript exercícios de arrays

---
+++ Javascript Array/index.js
@@ -0,0 +1,24 @@
+/*
+1 - Armazenar 10 nomes em um vetor e imprimir uma lista numerada
+*/
+/*
+const nomesArray = [];
+
+for(let i = 0; i < 10; i++) {
+    nome = window.prompt("Entre com um nome: ");
+    nomesArray.push(nome);
+}
+
+for(let i = 0; i < 10; i++) {
+    console.log(nomesArray[i]);
+} */
+
+
+
+/*
+
+*/
+
+
+
+
