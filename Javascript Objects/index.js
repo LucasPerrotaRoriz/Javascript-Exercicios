e6f6129a4bc0fc31498b58153c7cc78d02e5ae8f
LucasPerrotaRoriz <LucasPR1998@protonmail.com>
2022-05-18 13:52:18 -0300

    Javascript exercícios de objetos

---
+++ Javascript Objects/index.js
@@ -0,0 +1,32 @@
+
+
+/*
+2 - Armazenar 15 números inteiros em um vetor e guardar em um objeto o número
+dizendo se o número é par ou ímpar
+*/
+/*
+function random() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+const numArray = []
+
+for(let i = 0; i < 15; i++) {
+    numArray.push(random());
+}
+
+const arrayObj = [];
+
+for(let i = 0; i < 15; i++) {
+    const numObj = {};
+    if(numArray[i] % 2 === 0) {
+        numObj.num = numArray[i];
+        numObj.prop = 'Par';
+        arrayObj.push(numObj);
+    } else {
+        numObj.num = numArray[i];
+        numObj.prop = 'Impar';
+        arrayObj.push(numObj);
+    }
+}
+console.log(arrayObj); */
