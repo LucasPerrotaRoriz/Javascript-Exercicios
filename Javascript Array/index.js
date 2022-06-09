d55860967de4804cfc2bfb105069d865b8974014
LucasPerrotaRoriz <LucasPR1998@protonmail.com>
2022-06-09 20:26:55 -0300

    exercicio array parte 2

--- Javascript Array/index.js
+++ Javascript Array/index.js
@@ -454,21 +454,75 @@ printEveryNthValue(array,value);
 https://www.rithmschool.com/courses/javascript/javascript-arrays-exercise
 ##################################################
 */
-
-/*
 let people = ["Greg", "Mary", "Devon", "James"];
 
-Using a loop, iterate through this array and console.log all of the people.
-Write the command to remove "Greg" from the array.
-Write the command to remove "James" from the array.
-Write the command to add "Matt" to the front of the array.
-Write the command to add your name to the end of the array.
-Using a loop, iterate through this array and after console.log-ing "Mary", exit from the loop.
-Write the command to make a copy of the array using slice. The copy should NOT include "Mary" or "Matt".
-Write the command that gives the indexOf where "Mary" is located.
-Write the command that gives the indexOf where "Foo" is located (this should return -1).
-Redefine the people variable with the value you started with. Using the splice command, remove "Devon" from the array and add "Elizabeth" and "Artie". Your array should look like this when you are done ["Greg", "Mary", "Elizabeth", "Artie", "James"].
-Create a new variable called withBob and set it equal to the people array concatenated with the string of "Bob".
+//Using a loop, iterate through this array and console.log all of the people.
+/*
+for(let i = 0; i < people.length; i++) {
+   console.log(people[i]);
+}
+*/
+
+//Write the command to remove "Greg" from the array.
+/*
+people.shift();
+console.log(people);
+*/
+
+//Write the command to remove "James" from the array.
+/*
+people.pop();
+console.log(people);
+*/
+
+//Write the command to add "Matt" to the front of the array.
+/*
+people.unshift('Matt');
+console.log(people);
+*/
+
+//Write the command to add your name to the end of the array.
+/*
+people.push('Arthur');
+console.log(people);
+*/
+
+//Using a loop, iterate through this array and after console.log-ing "Mary", exit from the loop.
+/*
+for(let value of people) {
+   if(value == 'Mary') {
+      break;
+   } else {
+      console.log(value);
+   }
+}*/
+
+//Write the command to make a copy of the array using slice. The copy should NOT include "Mary" or "Matt".
+/*
+let people2 = people.slice(2);
+console.log(people2);
+*/
+
+//Write the command that gives the indexOf where "Mary" is located.
+/*
+console.log(people.indexOf('Mary'));
+*/
+
+//Write the command that gives the indexOf where "Foo" is located (this should return -1).
+/*
+console.log(people.indexOf('Foo'));
+*/
+//Redefine the people variable with the value you started with. Using the splice command, remove "Devon" from the array and add "Elizabeth" and "Artie". Your array should look like this when you are done ["Greg", "Mary", "Elizabeth", "Artie", "James"].
+/*
+let people2 = ['Matt', 'Marry', 'Devon', 'Arthur'];
+people2.splice(2, 1, 'Elisabeth', 'Artie');
+console.log(people2);
+*/
+
+//Create a new variable called withBob and set it equal to the people array concatenated with the string of "Bob".
+/*
+let withBob = people.concat('Bob');
+console.log(withBob);
 */
 
 /* ------------------------------------------------------------------------------------------- */
