01e64cc13eddef956b736f9cf59f753dcb93f2ae
LucasPerrotaRoriz <LucasPR1998@protonmail.com>
2022-06-08 22:19:03 -0300

    .

--- Javascript String/index.js
+++ Javascript String/index.js
@@ -5,7 +5,17 @@ https://justacoding.blog/javascript-exercises-for-beginners-part-1-strings/
 */
 /*
 1. Reverse that string
-This one is a classic. Simply reverse the string without using reverse().
+This one is a classic. Simply reverse the string "I'm am a string" without using reverse().
+
+function reverseString(string) {
+    let reverse = "";
+    for(let i = string.length - 1; i >= 0; i--) {
+        reverse += string[i];
+    }
+    return reverse;
+}
+
+console.log(reverseString("I am a string"));
 */
 
 /*
@@ -13,13 +23,29 @@ This one is a classic. Simply reverse the string without using reverse().
 Another classic. You can apply your previous solution to help with this one.
 
 Does the given string spell out the same word when written out backwards? For example kayak.
+
+
+const isPalindrome = (string) => {
+    const len = string.length;
+
+    for(let i = 0; i < len / 2; i++) {
+        if(string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
+console.log(isPalindrome("mom"));
+console.log(isPalindrome("osso"));
+console.log(isPalindrome("madam"));
+console.log(isPalindrome("apple"));
 */
 
 /*
 3. Remove specific characters from a string
-What’s the most efficient way to strip specific characters from a string? Without using replace().
 
-Your function must strip multiple different characters from the provided string.
+let s = "I am an example string";
+console.log(s.replaceAll("a", "x"));
 */
 
 /*
@@ -34,6 +60,25 @@ For instance:
 xybbbefaaag
 
 Would return a, as a and b both occur 3 times each, respectively, but a comes before b alphabetically.
+
+
+let string = "abcddefg";
+let string2 = "heggbdeff";
+
+const mostCommonCharacter = (string) => {
+    const results = string.split('').reduce((tally, val) => {
+        if (tally[val]) {
+          tally[val]++;
+        } else {
+          tally[val] = 1;
+        }
+        return tally;
+      }, {});
+      console.log(results);
+}
+
+console.log(mostCommonCharacter(string));
+console.log(mostCommonCharacter(string2));
 */
 
 /*
